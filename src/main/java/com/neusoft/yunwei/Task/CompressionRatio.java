2d193a1569764b9b40ea66af679501f7003904fd
zhangyuan <zhang.yuan_neu>
2022-10-20 15:56:02 +0800

    测试

---
+++ src/main/java/com/neusoft/yunwei/Task/CompressionRatio.java
@@ -0,0 +1,13 @@
+package com.neusoft.yunwei.Task;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CompressionRatio {
+
+}
